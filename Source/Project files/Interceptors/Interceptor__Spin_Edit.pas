92ffd8a5f7713c0bd6271bb9729e845c3eaf88e4
Jacek Mulawka <78687840+jacek-mulawka@users.noreply.github.com>
2025-04-30 22:15:36 +0200

    Add files via upload

--- Source/Project files/Interceptors/Interceptor__Spin_Edit.pas
+++ Source/Project files/Interceptors/Interceptor__Spin_Edit.pas
@@ -1,7 +1,5 @@
 unit Interceptor__Spin_Edit;{21.Lut.2024}
 
-  // After setting the value in the object inspector the value may be outside the min-max range.
-
 interface
 
 uses
@@ -18,16 +16,39 @@ uses
 type
   TSpinEdit = class( Vcl.Samples.Spin.TSpinEdit )
   private
-    FIncrement : currency;
-    FMinValue : currency;
-    FMaxValue : currency;
+    FCheck_Value__Max,
+    FCheck_Value__Min,
+    FValue_Type_Float
+      : boolean;
+
+    FDecimal : integer; // Number of digits after the decimal point.
+
+    FIncrement,
+    FMaxValue,
+    FMinValue
+      : currency;
 
     function CheckValue( NewValue : currency ) : currency;
+
+    procedure Check_Value__Max__Set( const check_value__max_f : boolean );
+    procedure Check_Value__Min__Set( const check_value__min_f : boolean );
+
     procedure CMExit( var Message : Vcl.Controls.TCMExit ); message Vcl.Controls.CM_EXIT;
-    function GetValue() : currency;
-    procedure SetValue( NewValue : currency );
+    function GetValue() : currency; overload;
+    function GetValue( const value__default_f : currency ) : currency; overload;
+
+    procedure Decimal__Set( const decimal_f : integer );
 
     function Increment_Value_Calculate() : currency;
+
+    procedure Max__Value__Set( const new_value__max_f : currency );
+    procedure Min__Value__Set( const new_value__min_f : currency );
+
+    procedure Sel__Start__Set( increment_value_f : currency );
+
+    procedure SetValue( NewValue : currency );
+
+    procedure Value_Type_Float__Set( const value_type_float_f : boolean );
   protected
     procedure DownClick( Sender : TObject ); override;
     function IsValidChar( Key : Char ) : Boolean; virtual;
@@ -35,17 +56,22 @@ type
     procedure KeyPress( var Key : Char ); override;
     procedure UpClick( Sender : TObject ); override;
   public
-    check_value__max,
-    check_value__min,
-    value_type_float
-      : boolean;
+    increment__smart : boolean;
 
     constructor Create( AOwner: System.Classes.TComponent ); override;
   published
+    property Check_Value__Max : boolean read FCheck_Value__Max write Check_Value__Max__Set;
+    property Check_Value__Min : boolean read FCheck_Value__Min write Check_Value__Min__Set;
+
+    property Decimal : integer read FDecimal write Decimal__Set;
+
     property Increment : currency read FIncrement write FIncrement;
-    property MaxValue : currency read FMaxValue write FMaxValue;
-    property MinValue : currency read FMinValue write FMinValue;
+
+    property MaxValue : currency read FMaxValue write Max__Value__Set;
+    property MinValue : currency read FMinValue write Min__Value__Set;
+
     property Value : currency read GetValue write SetValue;
+    property Value_Type_Float : boolean read FValue_Type_Float write Value_Type_Float__Set;
   end;
 
   TCustomEdit_Helper = class helper for Vcl.StdCtrls.TCustomEdit
@@ -64,10 +90,16 @@ begin
   inherited Create( AOwner );
 
 
-  Self.check_value__max := false;
-  Self.check_value__min := false;
-  Self.Increment := 1;
-  Self.value_type_float := false;
+  Self.FCheck_Value__Max := false;
+  Self.FCheck_Value__Min := false;
+  Self.FDecimal := 0; // No limits for Table__Data_Filter.
+  Self.FIncrement := 1;
+  Self.FMinValue := 0;
+  Self.FMaxValue := 0;
+  Self.FValue_Type_Float := false;
+  Self.increment__smart := true;
+
+  Self.SetValue( Self.GetValue() );  // To set display format.
 
 end;
 
@@ -76,13 +108,37 @@ begin
 
   Result := NewValue;
 
-  if   ( Self.check_value__min )
+
+  if    ( Self.FValue_Type_Float )
+    and ( Self.FDecimal >= 0 ) then
+    Result := System.Math.RoundTo( Result, -Self.FDecimal );
+
+
+  if   ( Self.FCheck_Value__Min )
    and ( NewValue < Self.MinValue ) then
    Result := Self.MinValue;
 
-  if   ( Self.check_value__max )
+  if   ( Self.FCheck_Value__Max )
    and ( NewValue > Self.MaxValue  ) then
    Result := Self.MaxValue;
+
+end;
+
+procedure TSpinEdit.Check_Value__Max__Set( const check_value__max_f : boolean );
+begin
+
+  Self.FCheck_Value__Max := check_value__max_f;
+
+  Self.SetValue( Self.GetValue() );
+
+end;
+
+procedure TSpinEdit.Check_Value__Min__Set( const check_value__min_f : boolean );
+begin
+
+  Self.FCheck_Value__Min := check_value__min_f;
+
+  Self.SetValue( Self.GetValue() );
 
 end;
 
@@ -99,11 +155,22 @@ begin
 
 end;
 
+procedure TSpinEdit.Decimal__Set( const decimal_f : integer );
+begin
+
+  Self.FDecimal := decimal_f;
+
+  Self.SetValue( Self.GetValue() ); // To correct number of digits after the decimal point.
+
+end;
+
 procedure TSpinEdit.DownClick( Sender : TObject );
 var
   zti_1 : integer;
 
-  ztr : currency;
+  ztc_1,
+  ztc_2
+    : currency;
 begin
 
   if Self.ReadOnly then
@@ -113,21 +180,34 @@ begin
 
       zti_1 := Self.SelStart;
 
-      ztr := Self.Value;
+      ztc_1 := Self.Value;
 
-      Self.Value := Self.Value - Increment_Value_Calculate();
+      ztc_2 := Increment_Value_Calculate();
+      Self.Value := Self.Value - ztc_2;
 
-      if    ( ztr >= 0 )
-        and ( Self.Value < 0 ) then
-        Self.SelStart := zti_1 + 1
-      else
-        Self.SelStart := zti_1;
+
+      if Self.increment__smart then
+        if Abs( ztc_2 ) >= 1 then
+          Sel__Start__Set( ztc_2 )
+        else
+          if    ( ztc_1 >= 0 )
+            and ( Self.Value < 0 ) then
+            Self.SelStart := zti_1 + 1
+          else
+            Self.SelStart := zti_1;
 
     end;
 
 end;
 
 function TSpinEdit.GetValue() : currency;
+begin
+
+  Result := Self.GetValue( Self.FMinValue );
+
+end;
+
+function TSpinEdit.GetValue( const value__default_f : currency ) : currency;
 var
   zts : string;
 begin
@@ -135,7 +215,7 @@ begin
   zts := Self.Text;
   zts := System.SysUtils.StringReplace( zts, ' ', '', [ rfReplaceAll ] );
 
-  Result := System.SysUtils.StrToFloatDef( zts, Self.FMinValue );
+  Result := System.SysUtils.StrToFloatDef( zts, value__default_f );
 
 end;
 
@@ -147,7 +227,7 @@ var
     : integer;
 begin
 
-  if Self.FIncrement <> 1 then
+  if not Self.increment__smart then
     begin
 
       Result := Self.FIncrement;
@@ -158,6 +238,7 @@ begin
 
 
   zti_1 := System.Math.Max(  Pos( ',', Self.Text ), Pos( '.', Self.Text )  );
+  zti_1 := System.Math.Max(  zti_1, Pos( FormatSettings.DecimalSeparator, Self.Text )  );
 
   if    ( zti_1 > 0 )
     and ( Self.SelStart >= zti_1 ) then
@@ -196,7 +277,7 @@ begin
         zti_2 := 1;
 
 
-      if Self.value_type_float then
+      if Self.FValue_Type_Float then
         begin
 
           if Self.Value < 0 then
@@ -229,26 +310,103 @@ begin
         end;
 
 
-      Result := Self.FIncrement * System.Math.Power( 10, zti_2 );
+      Result := System.Math.Power( 10, zti_2 );
 
     end;
 
 end;
 
 function TSpinEdit.IsValidChar( Key : Char ) : Boolean;
+
+  function Decimal_Length_Check() : boolean;
+  var
+    zti : integer;
+
+    zts : string;
+  begin
+
+    Result := true;
+
+
+    if Self.FDecimal < 0 then
+      Exit;
+
+
+    zti := System.Math.Max(  Pos( ',', Self.Text ), Pos( '.', Self.Text )  );
+    zti := System.Math.Max(  zti, Pos( FormatSettings.DecimalSeparator, Self.Text )  );
+
+
+    if    ( zti <= 0 )
+      and (  Length( Self.Text ) > 0  )
+      and (
+               ( Key = ',' )
+            or ( Key = '.' )
+            or ( Key = FormatSettings.DecimalSeparator )
+          ) then
+      begin
+
+         // No decimal separator and the char is decimal separator.
+
+        zts := Copy(  Self.Text, Self.SelStart + 1, Length( Self.Text )  );
+
+        if Length( zts ) > Self.FDecimal then
+          begin
+
+            Result := false;
+
+            Exit;
+
+          end;
+
+      end;
+
+
+    if   ( zti <= 0 ) // No decimal separator.
+      or ( Self.SelStart < zti ) then // Char before the decimal separator.
+      Exit;
+
+
+    zts := Copy(  Self.Text, zti + 1, Length( Self.Text )  );
+
+    if    ( Length( zts ) >= Self.FDecimal  )
+      and (  Key in [ '0'..'9' ] ) then
+      Result := false;
+
+  end;
+
 begin
 
   Result := inherited;
+
 
   if    ( Result )
     and (
              ( Key = #10 )
           or ( Key = #13 )
           or (
-                   ( not Self.value_type_float )
+                   ( not Self.FValue_Type_Float )
                and (
                         ( Key = ',' )
                      or ( Key = '.' )
+                     or ( Key = FormatSettings.DecimalSeparator )
+                   )
+             )
+          or (
+                   ( Self.FValue_Type_Float )
+               and (
+                        (
+                              ( Key = ',' )
+                          and (  Pos( ',', Self.Text ) > 0  )
+                        )
+                     or (
+                              ( Key = '.' )
+                          and (  Pos( '.', Self.Text ) > 0  )
+                        )
+                     or (
+                              ( Key = FormatSettings.DecimalSeparator )
+                          and (  Pos( FormatSettings.DecimalSeparator, Self.Text ) > 0  )
+                        )
+                     or ( not Decimal_Length_Check() )
                    )
              )
          ) then
@@ -276,6 +434,10 @@ begin
 end;
 
 procedure TSpinEdit.KeyPress( var Key : Char );
+var
+  zti : integer;
+  ztc : currency;
+  zts : string;
 begin
 
   if not Self.IsValidChar( Key ) then
@@ -286,26 +448,135 @@ begin
 
     end;
 
+
+  //if Key = ^V then // Ctrl + V.
+  //  begin
+  //
+  //    // Check paste values. //????
+  //
+  //    Key := #0;
+  //
+  //    ztc := Self.GetValue();
+  //    zti := Self.SelStart;
+  //
+  //    if Length( Self.SelText ) > 0 then
+  //      begin
+  //
+  //        Self.SelText := Vcl.Clipbrd.Clipboard.AsText;
+  //        zts := Self.Text;
+  //
+  //      end
+  //    else
+  //      begin
+  //
+  //        zts := Self.Text;
+  //        Insert( Vcl.Clipbrd.Clipboard.AsText, zts, Self.SelStart + 1 );
+  //
+  //      end;
+  //
+  //    zts := Trim(  StringReplace( zts, ' ', '', [ rfReplaceAll ] )  );
+  //    Self.Text := zts;
+  //    Self.SetValue(  Self.GetValue( ztc )  );
+  //
+  //    Self.SelStart := zti;
+  //
+  //  end;
+
+
   if Key <> #0 then
     Vcl.StdCtrls.TCustomEdit(Self).KeyPress( Key );
 
 end;
 
-procedure TSpinEdit.SetValue( NewValue : currency );
-var
-  ztr : currency;
+procedure TSpinEdit.Max__Value__Set( const new_value__max_f : currency );
 begin
 
-  ztr := Self.CheckValue( NewValue );
+  Self.FMaxValue := new_value__max_f;
+
+  Self.SetValue( Self.GetValue() );
+
+end;
+
+procedure TSpinEdit.Min__Value__Set( const new_value__min_f : currency );
+begin
+
+  Self.FMinValue := new_value__min_f;
+
+  Self.SetValue( Self.GetValue() );
+
+end;
+
+procedure TSpinEdit.Sel__Start__Set( increment_value_f : currency );
+var
+  zti : integer;
+
+  ztc : currency;
+
+  zts : string;
+begin
+
+  if    ( Self.increment__smart )
+    and ( increment_value_f >= 1 ) then
+    begin
+
+      zts := System.SysUtils.Trim(  System.SysUtils.FormatFloat( '### ### ### ### ### ### ##0', increment_value_f )  );
+      zti := Length( zts );
+
+      if zti <= Length( Self.Text ) then
+        Self.SelStart := Length( Self.Text ) - zti
+      else
+        Self.SelStart := 0;
 
 
-  if Self.value_type_float then
-    Self.Text := System.SysUtils.Trim(   System.SysUtils.FormatFloat(  '### ### ### ### ### ### ##0.0#############', Abs( ztr )  )   )
+      if    ( Self.SelStart = 0 )
+        and ( Self.Value < 0 ) then
+        Self.SelStart := Self.SelStart + 1;
+
+
+      if Self.SelStart > 0 then
+        begin
+
+          zti := System.Math.Max(  Pos( ',', Self.Text ), Pos( '.', Self.Text )  );
+          zti := System.Math.Max(  zti, Pos( FormatSettings.DecimalSeparator, Self.Text )  );
+
+          if zti > 0 then
+            begin
+
+              zti := Length( Self.Text ) - zti + 1;
+
+              if zti <= Self.SelStart then
+                Self.SelStart := Self.SelStart - zti
+              else
+                Self.SelStart := 0;
+
+
+              if    ( Self.SelStart = 0 )
+                and ( Self.Value < 0 ) then
+                Self.SelStart := Self.SelStart + 1;
+
+            end;
+
+        end;
+
+    end;
+
+end;
+
+procedure TSpinEdit.SetValue( NewValue : currency );
+var
+  ztc : currency;
+begin
+
+  ztc := Self.CheckValue( NewValue );
+
+
+  if Self.FValue_Type_Float then
+    Self.Text := System.SysUtils.Trim(   System.SysUtils.FormatFloat(  '### ### ### ### ### ### ##0.0#############', Abs( ztc )  )   )
   else
-    Self.Text := System.SysUtils.Trim(   System.SysUtils.FormatFloat(  '### ### ### ### ### ### ##0', Abs( ztr )  )   );
+    Self.Text := System.SysUtils.Trim(   System.SysUtils.FormatFloat(  '### ### ### ### ### ### ##0', Abs( ztc )  )   );
 
 
-  if ztr < 0 then
+  if ztc < 0 then
     Self.Text := Common.minus_sign_s_c + Self.Text;
 
 end;
@@ -314,7 +585,9 @@ procedure TSpinEdit.UpClick( Sender : TObject );
 var
   zti_1 : integer;
 
-  ztr : currency;
+  ztc_1,
+  ztc_2
+    : currency;
 begin
 
   if Self.ReadOnly then
@@ -324,17 +597,32 @@ begin
 
       zti_1 := Self.SelStart;
 
-      ztr := Self.Value;
+      ztc_1 := Self.Value;
 
-      Self.Value := Self.Value + Increment_Value_Calculate();
+      ztc_2 := Increment_Value_Calculate();
+      Self.Value := Self.Value + ztc_2;
 
-      if    ( ztr < 0 )
-        and ( Self.Value >= 0 ) then
-        Self.SelStart := zti_1 - 1
-      else
-        Self.SelStart := zti_1;
+
+      if Self.increment__smart then
+        if Abs( ztc_2 ) >= 1 then
+          Sel__Start__Set( ztc_2 )
+        else
+          if    ( ztc_1 < 0 )
+            and ( Self.Value >= 0 ) then
+            Self.SelStart := zti_1 - 1
+          else
+            Self.SelStart := zti_1;
 
     end;
+
+end;
+
+procedure TSpinEdit.Value_Type_Float__Set( const value_type_float_f : boolean );
+begin
+
+  Self.FValue_Type_Float := value_type_float_f;
+
+  Self.SetValue( Self.GetValue() );  // To set display format.
 
 end;
 
